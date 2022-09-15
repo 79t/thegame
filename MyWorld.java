54139852faaf34de7b7378942d17b9a74c8696c5
suhas <hi@suhas.one>
2022-09-15 15:28:10 -0500

    and so it begins

---
+++ MyWorld.java
@@ -0,0 +1,17 @@
+import mayflower.*;
+
+
+public class MyWorld extends World {
+
+    public MyWorld()
+    {
+
+        Mayflower.showBounds(true);
+
+    }
+
+    public void act()
+    {
+    }
+
+}
