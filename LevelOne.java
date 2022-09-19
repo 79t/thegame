a07e8fd0eb61acb9bdb67053c15f93985967357e
suhas <hi@suhas.one>
2022-09-19 16:17:05 -0500

    level one

--- LevelOne.java
+++ LevelOne.java
@@ -3,7 +3,8 @@ public class LevelOne extends World {
 
     public LevelOne()
     {
-
+        // TODO: Build Overworld
+        //      objects, hazards, etc
         Mayflower.showBounds(true);
         showText("this is the world", 350, 250, Color.BLACK);
         showText("press w to win; l to lose", 350, 350, Color.BLACK);
@@ -15,6 +16,7 @@ public class LevelOne extends World {
 
     public void act()
     {
+
         if (Mayflower.isKeyPressed(Keyboard.KEY_W)) {
             World win = new GameOverWin();
             Mayflower.setWorld(win);
