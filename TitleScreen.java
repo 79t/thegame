9cd4935bc83db3adf9af31bf706bcc562a6094f9
alexyao <alexyao123@icloud.com>
2022-09-21 15:48:15 -0500

    zombie class and gif

--- TitleScreen.java
+++ TitleScreen.java
@@ -2,10 +2,13 @@ import mayflower.*;
 
 public class TitleScreen extends World {
 
+    private Zombie zombie;
     public TitleScreen() {
         setBackground("assets/BG/BG.png");
         showText("GAME", 350, 250, Color.BLACK);
         showText("Press P to play", 350, 350,Color.BLACK);
+        zombie = new Zombie();
+        addObject(zombie, 400, 100);
     }
     public void act() {
         if (Mayflower.isKeyPressed(Keyboard.KEY_P) ) {
