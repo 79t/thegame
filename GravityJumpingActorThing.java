7480abb581e30557f20d2e3da8de8dc472e01e78
suhas <hi@suhas.one>
2022-09-23 15:24:33 -0500

    Create GravityJumpingActorThing.java

---
+++ GravityJumpingActorThing.java
@@ -0,0 +1,39 @@
+import mayflower.*;
+
+public class GravityActor extends Actor {
+    private boolean isJumping;
+    public GravityActor() {
+        boolean isJumping = false;
+    }
+    public void act() {
+        if (!isJumping) setLocation(getX(), getY() + 1);
+        if (isBlocked()) setLocation(getX(), getY() - 1);
+
+        if (Mayflower.isKeyPressed(Keyboard.KEY_SPACE))
+        {
+            setLocation(getX(), getY()+1);
+            if (isBlocked()) {
+                isJumping = true;
+                Timer jumpTimer = new Timer(4000);
+                while (!jumpTimer.isDone()) {
+                    setLocation(getX(), getY() - 1);
+                }
+                isJumping = false;
+            }
+            setLocation(getX(), getY() - 1);
+
+        }
+    }
+
+    private boolean isBlocked() {
+        return isTouching(Block.class);
+    }
+
+    public boolean isFalling() {
+        boolean ret;
+        setLocation(getX(), getY() + 1);
+        ret = isTouching(Block.class);
+        setLocation(getX(), getY() - 1);
+        return !ret;
+    }
+}
