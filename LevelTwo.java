0924d9290d1a47a79b8936b0799ccd3d9fb1887e
Alex Yao <alexyao123@icloud.com>
2022-09-25 13:36:39 -0500

    I FUCKING FIXED GRAVITY

---
+++ LevelTwo.java
@@ -0,0 +1,46 @@
+import mayflower.*;
+
+public class LevelTwo extends World {
+
+    private String[][] tiles;
+    public LevelTwo()
+    {
+        Mayflower.showBounds(true);
+        setBackground("assets/BG/overworldBG.png");
+
+        // TODO: Build Overworld
+        //      objects, hazards, etc
+        Mayflower.showBounds(true);
+        showText("this is the world", 350, 250, Color.BLACK);
+        showText("press w to win; l to lose", 350, 350, Color.BLACK);
+
+        tiles = new String[6][8];
+        int bc = 0;
+        for (int i = 0; i < tiles[0].length; i++) {
+            for (int j = 0; j < tiles.length; j++) {
+                if (j == 5) {
+                    addObject(new Block(), i * 100, j * 100);
+                }
+            }
+        }
+        addObject(new Block(), 400, 400);
+        System.out.println(bc);
+
+        addObject(new Cat(),0, 300);
+
+
+    }
+
+    public void act()
+    {
+
+        if (Mayflower.isKeyPressed(Keyboard.KEY_W)) {
+            World win = new GameOverWin();
+            Mayflower.setWorld(win);
+        } else if (Mayflower.isKeyPressed(Keyboard.KEY_L)) {
+            World lose = new GameOverLose();
+            Mayflower.setWorld(lose);
+        }
+    }
+
+}
