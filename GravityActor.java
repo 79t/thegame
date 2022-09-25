83ab3156764f9165dacb14995cd6933c49f17129
Alex Yao <alexyao123@icloud.com>
2022-09-25 13:38:22 -0500

    no doulbe jupm

---
+++ GravityActor.java
@@ -0,0 +1,35 @@
+import mayflower.*;
+
+public class GravityActor extends Actor {
+    private float gravity = 0.1f;
+    private float velY;
+    private boolean jumping = false;
+
+    public void act() {
+        if (isFalling() || jumping) {
+            velY += gravity;
+            setLocation(getX(), getY() + velY);
+        }
+
+        if (isBlocked()) {
+            setLocation(getX(), getY() - velY);
+            velY = 0;
+        }
+
+        if (Mayflower.isKeyPressed(Keyboard.KEY_SPACE) && isBlocked()) {
+            velY = -5;
+        }
+    }
+
+    private boolean isBlocked() {
+        return isTouching(Block.class);
+    }
+
+    public boolean isFalling() {
+        boolean ret;
+        setLocation(getX(), getY() + 1);
+        ret = isTouching(Block.class);
+        setLocation(getX(), getY() - 1);
+        return !ret;
+    }
+}
