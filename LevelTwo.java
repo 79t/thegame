5e2fcded7e30e7a1f7be497a7a4f3d46e556b714
suhas <hi@suhas.one>
2022-09-30 15:14:13 -0500

    manual change merging

--- LevelTwo.java
+++ LevelTwo.java
@@ -1,5 +1,6 @@
 import mayflower.*;
 
+// this is the 2nd level
 public class LevelTwo extends World {
 
     private String[][] tiles;
@@ -13,30 +14,29 @@ public class LevelTwo extends World {
         Mayflower.showBounds(true);
         setBackground("assets/BG/nethre.jpeg");
 
-        // TODO: Build Overworld
-        //      objects, hazards, etc
         Mayflower.showBounds(true);
         t.showText("this is world 2", 350, 250);
         t.showText("press w to win; l to lose", 350, 350);
 
+        // make tiles, add blocks
         tiles = new String[6][8];
-        int bc = 0;
         for (int i = 0; i < tiles[0].length; i++) {
             for (int j = 0; j < tiles.length; j++) {
                 if (j == 5) {
-                    addObject(new Block(), i * 50, j * 100);
-                    addObject(new Lava(), i * 50 + 350, j * 100);
+                    addObject(new Block(), i * 100, j * 100);
                 }
             }
         }
 
+        // add climbing ladder
         addObject(new Block(), 475, 375);
-        addObject(new Block(), 690, 250);
         addObject(new Emerald(), 100, 400);
+        addObject(new Block(), 400, 400);
+
         NetherPortal n = new NetherPortal();
         addObject(n, 700, 120);
-        System.out.println(bc);
 
+        // make cat with same score and health as LevelOne
         c = new Cat(score, health);
 
         addObject(c,0, 300);
